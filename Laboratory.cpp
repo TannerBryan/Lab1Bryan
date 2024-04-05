e36578c3f0b16819ee75915d252ecac6d3334ddd
Tanner Bryan <tanner.bryan@rocket.utoledo.edu>
2024-04-05 12:10:57 -0400

    first commit

---
+++ Laboratory.cpp
@@ -0,0 +1,24 @@
+/*
+* Program:  <First Laboratory>
+* Author:   <Tanner Bryan>
+* Date:     <1/24/2024>
+* 
+*/ 
+
+#include <iostream> 
+#include <string>
+using namespace std;
+
+
+int main(void) {
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";
+
+	cout << "Who's on First? \n.";
+	return 0;
+}
